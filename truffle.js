bf18a72badaa72817d46a339110a67ede77677fb
Kostiantyn Smyrnov <kostysh@gmail.com>
2020-11-15 02:41:01 +0200

    added .openzeppelin with project.json

--- truffle.js
+++ truffle.js
@@ -2,55 +2,55 @@ var HDWalletProvider = require('@truffle/hdwallet-provider');
 
 // Network configuration factory
 const getInfuraConfig = (networkName, networkId) => {
-    var keys = {};
+  var keys = {};
 
-    try {
-        keys = require('./keys.json');
-    } catch (err) {
-        console.log('could not find ./keys.json');
-    }
+  try {
+    keys = require('./keys.json');
+  } catch (err) {
+    console.log('could not find ./keys.json');
+  }
 
-    return {
-        network_id: networkId, // eslint-disable-line camelcase
-        provider: () => {
-            return new HDWalletProvider(
-                keys.keys,
-                `https://${networkName}.infura.io/v3/` + keys.infura_projectid,
-                0,
-                10
-            );
-        },
-        gas: 7900000,
-        gasPrice: 100000000000
-    };
+  return {
+    network_id: networkId, // eslint-disable-line camelcase
+    provider: () => {
+      return new HDWalletProvider(
+        keys.keys,
+        `https://${networkName}.infura.io/v3/` + keys.infura_projectid,
+        0,
+        10
+      );
+    },
+    gas: 7900000,
+    gasPrice: 100000000000
+  };
 };
 
 module.exports = {
-    plugins: [
-        'solidity-coverage'
-    ],
+  plugins: [
+    'solidity-coverage'
+  ],
 
-    networks: {
-        development: {
-            host: 'localhost',
-            port: 8545,
-            network_id: '*', // eslint-disable-line camelcase
-            gas: 8000000,
-            gasPrice: 20000000000
-        },
-        main: getInfuraConfig('mainnet', 1),
-        ropsten: getInfuraConfig('ropsten', 3)
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // eslint-disable-line camelcase
+      gas: 8000000,
+      gasPrice: 20000000000
     },
+    main: getInfuraConfig('mainnet', 1),
+    ropsten: getInfuraConfig('ropsten', 3)
+  },
 
-    compilers: {
-        solc: {
-            version: '0.6.6',
-            settings: {
-                optimizer: {
-                    enabled: true,
-                    runs: 200
-                }
-            }
+  compilers: {
+    solc: {
+      version: '0.6.6',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
         }
+      }
     }
+  }
 };
